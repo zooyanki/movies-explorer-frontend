80aad8a2d2a62ee7b5446e6260dc556a78d2450c
zooyanki <65367400+zooyanki@users.noreply.github.com>
2021-02-27 20:27:33 +0300

    validation

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
+import useInput from '../Validation/Validation'
+
+
 import WithForm from '../WithForm/WithForm';
 
 
 function Register() {
+    const name = useInput('',{isEmpty:true, minLength: 2, maxLength:30});
+ 
     return(
         <WithForm header={`Добро пожаловать!`} buttonText={`Зарегистрироваться`} linkText={`Войти`} link={`/signin`} textReg={`Уже`}>
             <p className="withform__text">Имя</p>
-            <input className="withform__input" name="name" type="text" minLength="2" maxLength="30" required/>
-            <p className="withform__validation">Валидация</p>
+            <input className={`withform__input ${(name.isDirty && name.minLengthError || name.isDirty && name.maxLengthError) && 'withform__input_validation'}`} name="name" type="text" value={name.value} onBlur={name.onBlur} onChange={name.onChange} required/>
+            {(name.isDirty && name.minLengthError || name.isDirty && name.maxLengthError) && <p className="withform__validation">{name.errorMessage}</p>}
+
         </WithForm> 
     )
 }
---
+++ src/components/Validation/Validation.js
@@ -0,0 +1,112 @@
+import {useEffect, useState} from 'react';
+
+const useValidation = (value, validations) => {
+    const [isEmpty, setIsEmpty] = useState(true);
+    const [minLengthError, setMinLengthError] = useState(false);
+    const [maxLengthError, setMaxLengthError] = useState(false);
+    const [emailError, setEmailError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState(true);
+    const [inputValid, setInputValid] = useState(false);
+
+
+    useEffect(
+        () => {
+            for (const validation in validations) {
+                switch(validation) {
+                
+                    case 'minLength':
+                        if(value.length < validations[validation]) {
+                            setMinLengthError(true)
+                            setErrorMessage('Пустое поле')
+                            if (value) {
+                                setErrorMessage(`Менее ${validations.minLength} символов`)
+                            }
+                        } else {
+                            setMinLengthError(false)
+                        }                         
+                        break;
+
+                    case 'maxLength':
+                        if(value.length > validations[validation]) {
+                            setMaxLengthError(true)
+                            if (value) {
+                                setErrorMessage(`Более ${validations.maxLength} символов`)
+                            }                            
+                        } else {
+                            setMaxLengthError(false)
+                        }                         
+                        break;
+
+                    case 'isEmpty':
+                        if(value){ 
+                            setIsEmpty(false)
+                        } else {
+                            setIsEmpty(true)
+                            setErrorMessage('Пустое поле')
+                        }
+                        break;
+
+                    case 'emailError':
+                        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                        if (!re.test(String(value).toLowerCase())) {
+                            setEmailError(true)
+                            setErrorMessage('Пустое поле')
+                            if (value) {
+                                setErrorMessage('Не корректный e-mail')
+                            }
+                        } else {
+                            setEmailError(false);
+                        }
+                        break;
+
+                    default:
+                }
+            }
+        },
+        [value]
+    )
+
+    useEffect(
+        () => {
+            if(minLengthError || isEmpty || emailError || maxLengthError) {
+                setInputValid(false)
+            } else {
+                setInputValid(true)
+            }
+        },
+        [minLengthError, isEmpty, emailError, maxLengthError]
+    )
+
+    return {
+        isEmpty,
+        minLengthError,
+        maxLengthError,
+        errorMessage,
+        emailError,
+        inputValid,        
+    }
+}
+
+const useInput = (initialValue, validations) => {
+    const [value, setValue] = useState(initialValue);
+    const [isDirty, setIsDirty] = useState(false);
+    const valid = useValidation(value, validations)
+    const onChange = (e) => {
+        setValue(e.target.value)
+    }
+
+    const onBlur = (e) => {
+        setIsDirty(true)
+    }
+
+    return {
+        value,
+        onChange,
+        onBlur,
+        isDirty,
+        ...valid
+    }
+}
+
+
+export default useInput;
